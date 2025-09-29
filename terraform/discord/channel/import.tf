7c493be4817a0c299e983a1f69d6bf52cee885c5
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2025-09-29 13:42:02 +0900

    discordの設定

---
+++ terraform/discord/channel/import.tf
@@ -0,0 +1,44 @@
+import {
+  to = discord_category_channel.this["text"]
+  id = 1310915420089483295
+}
+
+import {
+  to = discord_category_channel.this["voice"]
+  id = 1310915420089483296
+}
+
+import {
+  to = discord_category_channel.this["admin"]
+  id = 1422020501135691776
+}
+
+import {
+  to = discord_text_channel.this["lobby"]
+  id = 1310915420089483297
+}
+
+import {
+  to = discord_text_channel.this["chat"]
+  id = 1310917382553731092
+}
+
+import {
+  to = discord_text_channel.this["meetup"]
+  id = 1311162974702014465
+}
+
+import {
+  to = discord_text_channel.this["organaizers"]
+  id = 1422020797391700140
+}
+
+import {
+  to = discord_voice_channel.this["chat"]
+  id = 1310915420538011678
+}
+
+import {
+  to = discord_voice_channel.this["lt"]
+  id = 1409816431847280660
+}
