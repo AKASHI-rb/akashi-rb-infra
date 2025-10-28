3dc73c34fca9c525e25532a9e32bbc60ef8cb2bf
muso <46985135+nishioka-shinji@users.noreply.github.com>
2025-10-28 20:24:02 +0900

    Update text_channel.tf

--- terraform/discord/channel/text_channel.tf
+++ terraform/discord/channel/text_channel.tf
@@ -21,7 +21,7 @@ locals {
       category = discord_category_channel.this["admin"].id
     }
     "3d-printer" = {
-      name     = "3d-printer"
+      name     = "3Dプリンタ"
       position = 4
       category = discord_category_channel.this["text"].id
     }
